10954e47ca5b1e217e90fded9a002560b00e938c
oldwall <18243084457@163.com>
2017-01-02 22:12:05 +0800

    Jinja2 fix

--- hello.py
+++ hello.py
@@ -1,27 +1,41 @@
 #-*- coding:utf-8 -*-
 
-#---- fix UnicodeDecodeError -----
-import sys
-reload(sys)
-sys.setdefaultencoding('utf8')
-#---------------------------------
+from datetime import datetime
+from flask import Flask,render_template
+from flask_bootstrap import Bootstrap
+from flask_moment import Moment
 
-from flask import Flask
+
 app = Flask(__name__)
+bootstrap = Bootstrap(app)
+mament = Moment(app)
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'), 500
+	
 
 @app.route('/')
-def hello():
-    return '<h1>Grey Wall</h1>'
+def index():
+    return render_template('index.html',current_time=datetime.utcnow())
+
 
 @app.route('/mei')
 def mei():
-    return '<h1>小湄天仙 *^ ^*</h1>'
+    return render_template('mei.html')
 
 	
 @app.route('/sb/<name>')
 def sb(name):
-    return '<h1>%s 是 傻逼！<h1>' % name
+    return render_template('sb.html',name=name)
+
 	
 if __name__ == '__main__':
-	app.run(host='0.0.0.0',port=5000,debug = True)
+	app.run(host='0.0.0.0',port=80,debug = True)
 
